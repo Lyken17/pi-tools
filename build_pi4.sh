7b95b61eac51efdacae113130cc5b813d30b7d32
Ligeng Zhu <Lyken17@users.noreply.github.com>
2021-08-14 00:04:29 +0800

    Update build_pi4.sh

--- build_pi4.sh
+++ build_pi4.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 export TVM_VERSION=5140d90;
-docker build -f pi4.dockerfile -t \
+docker build . -f pi4.dockerfile -t \
     lyken/pi-tools:pi4-v0.8.0-${TVM_VERSION} \
     --build-arg TVM_VERSION=${TVM_VERSION}
 
